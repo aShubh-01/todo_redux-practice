76379bede0a62da84008711b2fbd70f00c17821e
Ashubh01 <shubhamdhokare01@gmail.com>
2024-08-05 20:29:11 +0530

    Finished todo project with basic state managment using redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import { useState } from 'react'
+import AddTodo from './components/AddTodo'
+import TodosList from './components/TodosList'
 import './App.css'
 
 function App() {
 
   return (
       <>
-        waszzupp
+        <div style={{ margin: '10px', marginTop: '20px'}}>
+          <div style={{
+              margin: '10px'
+          }}>
+            <AddTodo />
+          </div>
+          <div style={{
+            margin: '10px'
+          }}>
+            <h2>Your Todos</h2>
+            <TodosList />
+          </div>
+        </div>
       </>
   )
 }
